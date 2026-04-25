6ff157aac2a2902e8139dc4c28877e26b236fbae
Matiazz07 <damian.villota.2007y@gmail.com>
2026-04-25 16:01:53 -0500

    mostrar cualquier tabla

--- Tablas/Multiplicar.js
+++ Tablas/Multiplicar.js
@@ -1,12 +1,13 @@
 function generarTablas() {
     let contenedor = document.getElementById("table-responsive")
     let contenidoTabla = "<table><thead><tr><th>Nivel Sincronización</th><th>Poder de Franxx</th></tr></thead><tbody>";
-    let tabla = 5;
+    let tabla = document.getElementById("txtTabla").value;
+    let tablaEnt = parseInt(tabla);
     let titulo = document.getElementById("txtTitulo")
-    titulo.textContent = "¡TABLA DEL " + tabla + "!";
+    titulo.textContent = "¡TABLA DEL " + tablaEnt + "!";
     for (let i = 1; i <= 10; i++) {
-        resultado = Number(tabla) * i;
-        contenidoTabla += "<tr class='slide-in' style='--delay: 0.1s'><td>" + tabla + " x " + i + "</td><td><strong>" + resultado + "</strong></td></tr>";
+        resultado = tablaEnt * i;
+        contenidoTabla += "<tr class='slide-in' style='--delay: 0.1s'><td>" + tablaEnt + " x " + i + "</td><td><strong>" + resultado + "</strong></td></tr>";
     }
     contenedor.innerHTML = contenidoTabla;
 }
