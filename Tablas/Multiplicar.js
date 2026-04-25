4afde2101eb37fa3717f4cf389e8589717cff088
Matiazz07 <damian.villota.2007y@gmail.com>
2026-04-25 15:43:39 -0500

    mostrar tabla del 5

--- Tablas/Multiplicar.js
+++ Tablas/Multiplicar.js
@@ -1,8 +1,10 @@
 function generarTablas() {
     let contenedor = document.getElementById("table-responsive")
     let contenidoTabla = "<table><thead><tr><th>Nivel Sincronización</th><th>Poder de Franxx</th></tr></thead><tbody>";
+    let tabla = 5;
+    let titulo = document.getElementById("txtTitulo")
+    titulo.textContent = "¡TABLA DEL " + tabla + "!";
     for (let i = 1; i <= 10; i++) {
-        let tabla = 3;
         resultado = Number(tabla) * i;
         contenidoTabla += "<tr class='slide-in' style='--delay: 0.1s'><td>" + tabla + " x " + i + "</td><td><strong>" + resultado + "</strong></td></tr>";
     }
